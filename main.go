501a783b978ab9242ee6f8133442d5e6ce6b568c
Adrian L Thomas <adrian.thomas@gmail.com>
2020-07-15 19:52:15 +0100

    tidy up lint issues and errors

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"adrian-thomas.com/go-fetch/model"
+)
+
+func main() {
+	fmt.Println(model.ArtistImage{Width: 1, Height: 2, URL: "three"})
+}
---
+++ model/artist.go
@@ -0,0 +1,68 @@
+package model
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// ArtistImage represents an Artist's image
+type ArtistImage struct {
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+	URL    string `json:"url"`
+}
+
+func (ai ArtistImage) String() string {
+	return fmt.Sprintf("Height: %d, Width: %d, URL: %s", ai.Height, ai.Width, ai.URL)
+}
+
+// ArtistResponse represents a struct for the JSON response
+type ArtistResponse struct {
+	Followers struct {
+		Href  interface{} `json:"href"`
+		Total int         `json:"total"`
+	} `json:"followers"`
+	Genres     []string      `json:"genres"`
+	Images     []ArtistImage `json:"images"`
+	Name       string        `json:"name"`
+	Popularity int           `json:"popularity"`
+	Type       string        `json:"type"`
+	URI        string        `json:"uri"`
+}
+
+// Artist returns an Artist given an ArtistResponse
+func (ar ArtistResponse) Artist() Artist {
+	return Artist{
+		Name:       ar.Name,
+		Popularity: ar.Popularity,
+		Type:       ar.Type,
+		Followers:  ar.Followers.Total,
+		Genres:     ar.Genres,
+		Image:      ar.Images[0],
+	}
+}
+
+// Artist represents an unmarshalled Artist
+type Artist struct {
+	Name       string      `json:"name"`
+	Popularity int         `json:"popularity"`
+	Type       string      `json:"artist"`
+	Followers  int         `json:"followers"`
+	Genres     []string    `json:"genres"`
+	Image      ArtistImage `json:"image"`
+}
+
+// String formats the Artist struct
+func (a Artist) String() string {
+	// TODO get the image and ASCII it?
+	return fmt.Sprintf("Name: %s\nPopularity: %d\nType: %s\nFollowers: %d\nGenres:%v\nImage:%v", a.Name, a.Popularity, a.Type, a.Followers, a.Genres, a.Image)
+}
+
+// JSON returns a blob given an Artist
+func (a Artist) JSON() string {
+	aJSON, err := json.Marshal(a)
+	if err != nil {
+		return "ERROR!"
+	}
+	return string(aJSON)
+}
