9691219b79ee517e2e9789c8b6e78ec89ab79fbf
Adrian L Thomas <adrian.thomas@gmail.com>
2020-07-22 20:20:15 +0100

    download file from spotify and show

--- model/artist.go
+++ model/artist.go
@@ -3,7 +3,11 @@ package model
 import (
 	"encoding/json"
 	"fmt"
-	_ "image/png"
+	_ "image/jpeg"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
 
 	"github.com/qeesung/image2ascii/convert"
 )
@@ -16,6 +20,16 @@ type ArtistImage struct {
 }
 
 func (ai ArtistImage) String() string {
+	file, fileErr := ioutil.TempFile("", "example")
+	if fileErr != nil {
+		panic(fileErr)
+	}
+
+	err := DownloadFile(file.Name(), ai.URL)
+	if err != nil {
+		panic(err)
+	}
+
 	// Create convert options
 	convertOptions := convert.DefaultOptions
 	convertOptions.FixedWidth = 100
@@ -23,7 +37,7 @@ func (ai ArtistImage) String() string {
 
 	// Create the image converter
 	converter := convert.NewImageConverter()
-	return fmt.Sprintf(converter.ImageFile2ASCIIString("nurburgring.png", &convertOptions))
+	return fmt.Sprintf(converter.ImageFile2ASCIIString(file.Name(), &convertOptions))
 }
 
 // ArtistResponse represents a struct for the JSON response
@@ -74,4 +88,27 @@ func (a Artist) JSON() string {
 		return "ERROR!"
 	}
 	return string(aJSON)
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func DownloadFile(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
