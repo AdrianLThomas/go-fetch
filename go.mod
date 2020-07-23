f3a70a9d84630fbd97773ff5b26498b0ff5928ba
Adrian L Thomas <adrian.thomas@dazn.com>
2020-07-23 11:51:48 +0100

    added test for Artist()

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/aybabtme/rgbterm v0.0.0-20170906152045-cc83f3b3ce59 // indirect
+	github.com/google/go-cmp v0.5.1
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/qeesung/image2ascii v1.0.1
---
+++ model/artist_test.go
@@ -0,0 +1,52 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// TestArtist should return an Artist model when given an ArtistResponse
+func TestArtist(t *testing.T) {
+	// arrange
+	ar := ArtistResponse{
+		Followers: struct {
+			Href  interface{} "json:\"href\""
+			Total int         "json:\"total\""
+		}{
+			Total: 0,
+		},
+		Genres: []string{"rap", "rock"},
+		Images: []ArtistImage{
+			{
+				Height: 100,
+				Width:  100,
+				URL:    "http://www.example.com/artist.jpg",
+			},
+		},
+		Name:       "Adrian",
+		Popularity: 0,
+		Type:       "Some type",
+		URI:        "http://www.example.com/adrian",
+	}
+	expectedArtist := Artist{
+		Name:       "Adrian",
+		Popularity: 0,
+		Followers:  0,
+		Genres:     []string{"rap", "rock"},
+		Image: ArtistImage{
+			Height: 100,
+			Width:  100,
+			URL:    "http://www.example.com/artist.jpg",
+		},
+		Type: "Some type",
+	}
+
+	// act
+	artist := ar.Artist()
+
+	// assert
+	if !cmp.Equal(artist, expectedArtist) {
+		t.Error("artist and expectedArtist are not equal")
+	}
+}
